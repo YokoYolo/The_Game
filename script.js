11621a7da324a33f952e6b4b5eb4387253a43505
YokoYolo <y.ravlyk@gmail.com>
2018-06-28 17:24:45 -0400

    updated

--- script.js
+++ script.js
@@ -3,24 +3,23 @@
     var ctx = myCanvas.getContext('2d');
     var currentGame;
     var cow;
-    var newnumber;
+    var newNumber;
     var scoreText;
     var score = 0;
     var lives = 3;
     var livesText;
     var lifeLostText;
+    var currentNumbers =[];
+    var numberToDisplay;
+    var fallingNumbers = [];
  
-    
-
-
-
 var Cow = function(){
-  this.x = 240;
-  this.y = 500;
+  this.x = 500;
+  this.y = 650;
   this.width = 130;
-  this.height = 110;
+  this.height = 110; 
   this.img = './9cfa5a5e244027f5102f16140d2fbc7d.png';   
-  console.log(this);        
+  // console.log(this);        
 }
 
 
@@ -30,55 +29,97 @@ Cow.prototype.drawcow = function(){
   theImage = new Image();
   theImage.src = that.img;
   theImage.onload = function(){
-    ctx.drawImage(theImage, that.x, that.y, that.width, that.height)
+  ctx.drawImage(theImage, that.x, that.y, that.width, that.height)
   }
 }
 
-
-
-  var fallingNumber = function generateRandomNumber () {    
-  var sumNumber1 = Math.floor (1+ Math.random() * 9);
-  var sumNumber2 = Math.floor (1+ Math.random() * 9);   
-  var equation;
-  equation = sumNumber1 + sumNumber2;
+  function fallingNumber() {    
+  var sumNumberOne = Math.floor(1+ Math.random() * 9);
+  var sumNumberTwo = Math.floor(1+ Math.random() * 9); 
+  currentNumbers.push(sumNumberOne, sumNumberTwo); 
+  var equation = new  NewNumber(sumNumberOne + sumNumberTwo);
   var numberArray = [equation]
-  for(var i = 0; i < 3; i ++){
-    var newNumber = Math.floor (1+ Math.random() * 18);
+  for(var i = 0; i < 2; i ++){
+    var newNumber = new  NewNumber( Math.floor (1+ Math.random() * 18));
     numberArray.push(newNumber);
   }
-  return numberArray
+  fallingNumbers = numberArray;
+
+  for ( var i = 0; i < fallingNumbers.length; i++ ) {
+    fallingNumbers[i].move()
+  }
+
+if (sumNumberOne.x +50 >= sumNumberTwo.x && sumNumberOne  <= sumNumberTwo + sumNumberTwo.width 
+|| sumNumberOne.x +50 >= equation.x && sumNumberOne  <= equation.x + sequation.width
+|| sumNumberTwo.x +50 >= equation.x && sumNumberTwo  <= equation.x + sequation.width){
+    equation.x == Math.floor(Math.random(equation.x)) && sumNumberOne.x == Math.floor(Math.random(sumNumberOne.x)) &&
+    sumNumberTwo.x == Math.floor(Math.random(sumNumberTwo.x));
+}
+else { }
 
 };
 
 
-// var NewNumber = function(){
-//     this.x = 240;
-//     this.y = 500;
-//     this.width = 80;
-//     this.height = 80;
-//     console.log(this); 
-//    };
-//    var blah = new NewNumber();
-//    console.log("blah: ", blah);
-   
-// NewNumber.prototype.drawnewnumber = function(){
-//   console.log("hello: ", fallingNumber)
-//   ctx.fillText(fallingNumber, this.x, this.y, this.width, this.height);
-
-//   }
+var NewNumber = function(value){
+  this.value = value;
+    this.x = Math.floor(Math.random() * 700);
+    this.y = 50;
+    this.width = 35;
+    this.height = 35;  
+    ctx.font = "35px Arial";
+    ctx.fillStyle = "white";
+    // console.log(this); 
+  }
+  
 
 
-// blah.drawnewnumber();
+NewNumber.prototype.move = function (){
+  setInterval(() => {
+    this.y+=15;
+  },500)
+}
+
+
+function mainLoop(){
+
+setInterval(()=>{
+ctx.clearRect(0,0, myCanvas.width, myCanvas.height);
+
+for ( var i = 0; i < fallingNumbers.length; i++){
+  // console.log(fallingNumbers[i].value, fallingNumbers[i].x, fallingNumbers[i].y, fallingNumbers[i].width, fallingNumbers[i].height)
+  ctx.fillText(fallingNumbers[i].value, fallingNumbers[i].x, fallingNumbers[i].y, fallingNumbers[i].width, fallingNumbers[i].height);
+
+  if (currentGame.cow.x +130 >= fallingNumbers[i].x && currentGame.cow.x  <= fallingNumbers[i].x + fallingNumbers[i].width &&
+    currentGame.cow.y+110 >= fallingNumbers[i].y && currentGame.cow.y <= fallingNumbers[i].y){
+       console.log("collision", currentGame.cow, fallingNumbers[i])
+      }
+  else {
+    // console.log ("hello there");
+}
+
+
+
+}
+
+ctx.drawImage(theImage, currentGame.cow.x, currentGame.cow.y, currentGame.cow.width, currentGame.cow.height);
+
+},10)
+
+
+
+}
 
 
 document.getElementById("start-button").onclick = function(e) {
     currentGame = new Game();
     currentGame.cow = new Cow();
     currentGame.cow.drawcow();
-    // currentGame.newnumber = new NewNumber();
-    // currentGame.newnumber.drawnewnumber();
+    fallingNumber();
     document.getElementsByClassName('game-intro')[0].remove();
+    mainLoop();
   };
+
+
 
 Cow.prototype.move = function(whichKey){
   ctx.clearRect(this.x, this.y, this.width, this.height);
@@ -87,121 +128,27 @@ Cow.prototype.move = function(whichKey){
     case 'ArrowLeft':
     if(this.canMove(this.x - 15, this.y)){
         this.x -=15;
-    
     }
     break;
     case 'ArrowRight': 
     if(this.canMove(this.x + 15, this.y)){
         this.x +=15;
-     
-    }
-  };
-
-  ctx.drawImage(theImage, this.x, this.y, this.width, this.height);
+    }};
+  
 };
 
-Cow.prototype.canMove = function(futurex, futurey){
-//   if(
-//     futurex + this.width >= currentGame.obstacle.x &&
-//      futurex <= currentGame.obstacle.x + currentGame.obstacle.width &&
-//      futurey + this.height >= currentGame.obstacle.y &&
-//      futurey <= currentGame.obstacle.y + currentGame.obstacle.height
-//     ){
-    //   return false
-    // } else 
+
+Cow.prototype.canMove = function(futurex){
+
     if(
-      futurex + this.width >= 500 ||
-      futurex <= 0 ||
-      futurey + this.height >= 700 ||
-      futurey <= 0
+      futurex + this.width >= 700 ||
+      futurex <= 0 
     ){
       return false
     }
     return true;
-
 };
 
 document.onkeydown = function (e) {    
     currentGame.cow.move(e.key) 
-    
-    }
-
-
-// var Obstacle = function(x,y,width,height){
-//   this.x = x;
-//   this.y = y;
-//   this.height = height;
-//   this.width = width;
-// }
-
-// Obstacle.prototype.draw = function(){
-//   ctx.fillRect(this.x, this.y, this.width, this.height);
-// }
-
-
-
-    // this.badGuesses = [];
-    // this.goodGuesses = [];
-    // this.errorsLeft = 3;
-    // this.correctResults = [];
-  
-
- 
-
-
-
-//   fallingNumber.ptototype.move = function () {
-
-//   fallingNumber.getXPosition = function() {
-// 		return x;
-// 	};
-//   fallingNumber.getYPosition = function() {
-// 		return y;
-//   };
-  
-//   fallingNumber.getType = function (){
-// 		return type;
-//   }
-
-// };
-
-  
-
-
-
-// var Number = function () {
-
-//   this.numbers = function (i) {
-//   for (var i = 0; i <= 100; i++) {
-//       console.log(i);
-//     }}
-//   this.badGuesses = [];
-//   this.goodGuesses = [];
-//   this.errorsLeft = 3;
-//   this.correctResults = [];
-//   }
-
-// public void paint(printedNumber){
-//   g.setColor(Color.CYAN);
-//   g.fillRect(numXLocation,numYLocation,numSize,numSize);
-// }
-
-
-
-//  public FallingAnswer (){
-//   generateRandomXLocation();
-//   generateRandomNumber();
-//   generateRandomFallSpeed();
-// };
-
-
-// public void update(){
-
-
-//   if(squareYLocation >= canvas.height){
-//       generateRandomXLocation();
-//       generateRandomFallSpeed()
-//       numYLocation = -fallingNumberSize;
-//   }
-
-  //glyphicon glyphicon-heart
+    };
---
+++ script2.js
@@ -0,0 +1,36 @@
+
+
+
+// Car.prototype.canMove = function(futurex, futurey){
+//     if(
+//       futurex + this.width >= currentGame.obstacle.x &&
+//        futurex <= currentGame.obstacle.x + currentGame.obstacle.width &&
+//        futurey + this.height >= currentGame.obstacle.y &&
+//        futurey <= currentGame.obstacle.y + currentGame.obstacle.height
+//       ){
+//         return false
+//       } else if(
+//         futurex + this.width >= 500 ||
+//         futurex <= 0 ||
+//         futurey + this.height >= 700 ||
+//         futurey <= 0
+//       ){
+//         return false
+//       }
+//       return true;
+  
+//   }
+  
+  
+  
+//   var Obstacle = function(x,y,width,height){
+//     this.x = x;
+//     this.y = y;
+//     this.height = height;
+//     this.width = width;
+//   }
+  
+//   Obstacle.prototype.draw = function(){
+//     ctx.fillRect(this.x, this.y, this.width, this.height);
+//   }
+  
